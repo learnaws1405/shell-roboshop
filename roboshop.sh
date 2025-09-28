5ea26cf0db54ce8852a20ac1e6c7ad644c674e8f
rajiv <rajiv1405.r@gmail.com>
2025-09-29 00:28:37 +0530

    message

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ do
     Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0237e990ac2b2cf30 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
      if [ $instance != "frontend" ]; then 
-    IP=$(ws ec2 describe-instances --instance-ids $Instance_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     RECORD_NAME="$instance.$DOMAIN_NAME"
      else
     IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
