ed735480ae25757edbccf32191d92ac72f7700bf
rajiv <rajiv1405.r@gmail.com>
2025-09-29 21:50:12 +0530

    message

---
+++ catalogue-set.sh
@@ -0,0 +1,105 @@
+
+set -euo pipefall
+
+trap 'echo "Error in $LINENO, and command belongs to $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODBHOST="mongodb.daws86s.fun"
+SCRIPT_L=$PWD
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0| cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+
+if [ $USERID -ne 0 ]; then 
+ echo "Install with root user"
+ exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+echo " Script started : $(date)" 
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then 
+ echo -e "Install of $2 is $R FAILURE $N" | tee -a $LOG_FILE
+else
+ echo -e "Install of $2 is  $G SUCCESS $N" | tee -a $LOG_FILE
+fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+#VALIDATE "$?" "NodeJS lower version disabled"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+#VALIDATE "$?" "NodeJS 20 version enabled"
+
+dnf install nodejs -y &>>$LOG_FILE
+#VALIDATE "$?" "NodeJS 20 version installed"
+
+id roboshop
+if [ $? -ne 0 ]; then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    #VALIDATE "$?" "user created"
+else
+    echo -e "user aready created ::$Y SKIPPING $N"
+fi
+
+mkdir -p /app   
+#VALIDATE "$?" "Binary directory created"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+#VALIDATE "$?" "Binaries downloaded"
+
+cd /app
+#VALIDATE "$?" "changed directory"
+
+rm -rf /app/*
+unzip /tmp/catalogue.zip
+#VALIDATE "$?" "Unzipped bainaries"
+
+cd /app
+#VALIDATE "$?" "changed directory"
+
+npm install &>>$LOG_FILE
+#VALIDATE "$?" "installed dependecies"
+
+cp $SCRIPT_L/catalogue.repo /etc/systemd/system/catalogue.service
+#VALIDATE "$?" "Systemctl added"
+
+systemctl daemon-reload
+#VALIDATE "$?" "daemon reload"
+
+systemctl enable catalogue &>>$LOG_FILE
+#VALIDATE "$?" "systemctl enabled"
+
+systemctl start catalogue
+#VALIDATE "$?" "systemctl started nodejs applciation"
+
+systemctl status catalogue &>>$LOG_FILE
+if [ $? -ne 0 ]; then 
+   echo "PROBLEM in starting the service"
+   exit 1
+fi
+
+cp $SCRIPT_L/mongoclient.repo /etc/yum.repos.d/mongo.repo
+#VALIDATE "$?" "mongoclient repo added"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+#VALIDATE "$?" "mongoclient installed"
+
+
+
+INDEX=$(mongosh mongodb.dlearnaws.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -lt 0 ]; then
+ mongosh --host mongodb.dlearnaws.fun </app/db/master-data.js
+ VALIDATE "$?" "loading master data to mongoDB"
+else
+ echo -e "Master Data already loaded :: $Y SKIPPING $N"
+fi
+
+# /etc/systemd/system/catalogue.service
+# mongosh $MONGODBHOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')"
