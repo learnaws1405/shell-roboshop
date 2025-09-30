6fd94160cf86cf1f9d82576808a52daa27e70715
rajiv <rajiv1405.r@gmail.com>
2025-09-30 15:15:16 +0530

    message

---
+++ logrotation.sh
@@ -0,0 +1,30 @@
+#! /sbin/bash
+
+# count=5
+
+# while [ $count -gt 0]
+# do 
+#    count=$(($count -1))
+#    echo "count is $count"
+# done
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODBHOST="mongodb.daws86s.fun"
+SCRIPT_L=$PWD
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0| cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+SOURCE_DIR=/home/ec2-user/shell-roboshop/app-logs
+if [ -d $SOURCE_DIR ]; then
+FILESTODEL= $(find $SOURCE_DIR -name *.log -mtime +14)
+
+while IFS= read -r filepath
+do
+ echo " deleting the files ::::$filepath
+done <<< $SOURCE_DIR
