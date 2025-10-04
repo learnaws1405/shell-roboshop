d52f7fe256cc3617935afcb05a7df386413cf1ef
rajiv <rajiv1405.r@gmail.com>
2025-10-04 21:38:57 +0530

    message

--- usage.sh
+++ usage.sh
@@ -3,21 +3,17 @@
 disk_usage=$(df -hT | grep -iv filesystem)
 TH=2
 IP=$(curl -s  http://169.254.169.254/latest/meta-data/local-ipv4)
-MESSAGE1="''"
+MESSAGE="''"
 TO_TEAM="DEVOPS TEAM"
 while IFS= read -r line
 do 
     usage=$(echo $line|awk '{print $6}'| cut -d '%' -f1)
     part=$(echo $line|awk '{print $7}')
     if [ $usage -ge $TH ];then
-        MESSAGE1+="High usage on $part is $usage % <br>" 
+        MESSAGE+="High usage on $part is $usage % <br>" 
     fi
 done <<< $disk_usage
 
-echo -e "$MESSAGE1"
-
-MESSAGE=$(echo "$MESSAGE1" | tr -d '\r')
-
-echo  "$MESSAGE"
+echo -e "$MESSAGE"
 
 sh mail.sh "rajivklce@gmail.com" "High disk usage" "High  DISK USAGE" "$MESSAGE" "$IP" "$TO_TEAM"
