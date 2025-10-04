ba1d66fb9cd97e0d4292bf5774becbce0a4e1913
rajiv <rajiv1405.r@gmail.com>
2025-10-04 20:54:44 +0530

    message

--- mail.sh
+++ mail.sh
@@ -6,7 +6,9 @@ ALERT_TYPE=$3
 MESSAGE=$4
 IP=$5
 TO_TEAM=$6
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP/g" -e "s/MESSAGE/$MESSAGE/g" body.template)
+FORMATTED_BODY=$(printf '%s\n' "$MESSAGE" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP/g" -e "s/MESSAGE/$FORMATTED_BODY/g" body.template)
 
 {
 echo "To: $TO_ADDRESS"
@@ -15,4 +17,3 @@ echo "Content-Type: text/html"
 echo ""
 echo "$FINAL_BODY"
 } | msmtp "$TO_ADDRESS"
-
