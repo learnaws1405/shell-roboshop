9f74f1e8f5539a8038bb4caefcbe119aff3a59c9
rajiv <rajiv1405.r@gmail.com>
2025-10-04 21:09:29 +0530

    message

--- testdisk.sh
+++ testdisk.sh
@@ -4,3 +4,18 @@ disk_usage=$(df -hT | grep -iv filesystem)
 clean_output=$(echo "$disk_usage" | tr -d '\r')
 
 echo "$clean_output"
+
+TH=2
+IP=$(curl -s  http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE="''"
+TO_TEAM="DEVOPS TEAM"
+while IFS= read -r line
+do 
+    usage=$(echo $line|awk '{print $6}'| cut -d '%' -f1)
+    part=$(echo $line|awk '{print $7}')
+    if [ $usage -ge $TH ];then
+        MESSAGE+="High usage on $part is $usage % <br>" 
+    fi
+done <<< $clean_output
+
+echo -e "$MESSAGE"
