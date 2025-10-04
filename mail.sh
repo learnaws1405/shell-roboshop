4fb6709a9dbcac576680006bea1dd2c455354087
rajiv <rajiv1405.r@gmail.com>
2025-10-04 21:50:16 +0530

    message

--- mail.sh
+++ mail.sh
@@ -6,8 +6,14 @@ ALERT_TYPE=$3
 MESSAGE=$4
 IP=$5
 TO_TEAM=$6
+
+escape_sed() {
+  echo "$1" | sed -e 's/[&/\]/\\&/g'
+}
+
+FORMATTED_BODY=$(escape_sed "$MESSAGE")
 #FORMATTED_BODY=$(printf '%s\n' "$MESSAGE" | sed -e 's/[]\/$*.^[]/\\&/g')
-FORMATTED_BODY=$(echo $MESSAGE|sed -e 's/[&/\]/\\&/g')
+
 
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP/g" -e "s/MESSAGE/$FORMATTED_BODY/g" body.template)
 #FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP/g" -e "s/MESSAGE/$MESSAGE/g" body.template)
