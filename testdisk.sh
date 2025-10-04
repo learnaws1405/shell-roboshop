ff065c4ed7aa00794228fe8d5f86067b0e5ca277
rajiv <rajiv1405.r@gmail.com>
2025-10-04 21:25:27 +0530

    message

--- testdisk.sh
+++ testdisk.sh
@@ -5,7 +5,7 @@ disk_usage=$(df -hT | grep -iv filesystem)
 TH=2
 IP=$(curl -s  http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE1="''"
-TO_TEAM="DEVOPS TEAM"
+
 while IFS= read -r line
 do 
     usage=$(echo $line|awk '{print $6}'| cut -d '%' -f1)
@@ -17,6 +17,6 @@ done <<< $clean_output
 
 echo -e "$MESSAGE1"
 
-clean_output=$(echo "$MESSAGE1" | tr -d '\r')
+#clean_output=$(echo "$MESSAGE1" | tr -d '\r')
 
-echo "$clean_output"
+#echo "$clean_output"
