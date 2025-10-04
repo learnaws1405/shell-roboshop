9c26262e27af3377ea05309190cc09d77d2cd005
rajiv <rajiv1405.r@gmail.com>
2025-10-04 20:51:54 +0530

    message

--- mail.sh
+++ mail.sh
@@ -5,7 +5,7 @@ SUBJECT=$2
 ALERT_TYPE=$3
 MESSAGE=$4
 IP=$5
-
+TO_TEAM=$6
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP/g" -e "s/MESSAGE/$MESSAGE/g" body.template)
 
 {
--- usage.sh
+++ usage.sh
@@ -4,6 +4,7 @@ disk_usage=$(df -hT | grep -iv filesystem)
 TH=2
 IP=$(curl -s  http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE="''"
+TO_TEAM="DEVOPS TEAM"
 while IFS= read -r line
 do 
     usage=$(echo $line|awk '{print $6}'| cut -d '%' -f1)
@@ -15,4 +16,4 @@ done <<< $disk_usage
 
 echo -e "$MESSAGE"
 
-sh mail.sh "rajivklce@gmail.com" "High disk usage" "High  DISK USAGE" "$MESSAGE" "$IP" "TO_TEAM"
+sh mail.sh "rajivklce@gmail.com" "High disk usage" "High  DISK USAGE" "$MESSAGE" "$IP" "$TO_TEAM"
